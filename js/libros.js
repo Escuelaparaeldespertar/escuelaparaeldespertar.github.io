69a328bb00830128e49ef4c8df82e82436541041
Escuelaparaeldespertar <expansiondeldespertar@gmail.com>
2026-03-02 14:42:56 -0400

    Update libros.js

---
+++ js/libros.js
@@ -0,0 +1,7 @@
+const libros = [
+  { categoria:'Ken Wilber', titulo:'Gracia y Coraje', autor:'Ken Wilber', imagen:'Libros/gracia_y_coraje.jpg', url:'https://e.pcloud.link/...' },
+
+  { categoria:'Barbara Ann Brenan', titulo:'Manos que Curan', autor:'Barbara Ann Brennan', imagen:'Libros/Manosquecuran.jpg', url:'' }
+
+  // aquí todo lo demás exactamente igual
+];
